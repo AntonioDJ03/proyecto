b984cc8190342bd9eadcd9593d034e7ee4dfe72d
Antonio Jiménez Díaz <isc20350280@gmail.com>
2025-02-09 15:17:17 -0600

    Archivo de la base de datos

---
+++ tabla.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-02-2025 a las 22:10:53
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `basededatos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tabla`
+--
+
+CREATE TABLE `tabla` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `telefono` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tabla`
+--
+
+INSERT INTO `tabla` (`id`, `nombre`, `apellido`, `telefono`) VALUES
+(1, 'Antonio', 'Jimenez', 1234459897);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tabla`
+--
+ALTER TABLE `tabla`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tabla`
+--
+ALTER TABLE `tabla`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
